2bd4ba12afa4ca4fcecbcf5d31f6b6640ae27407
Hafliði Stefánsson <haffisaken@gmail.com>
2019-09-17 09:56:27 +0000

    beinagrind af leik
    Föll komin

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,2 +1,22 @@
-print('Bang')
-print("bong")
+def location(x, y):
+	
+	
+
+def moveMent(a):
+	
+	
+
+#def controls():
+
+#pos1, pos2 = controls()
+pos1 = 1
+pos2 = 1
+
+while location() != endir:
+	
+	
+	direction = location(pos1, pos2)
+	
+	
+	movement(direction)
+		 
