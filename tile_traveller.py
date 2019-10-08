9dab5642ecbdf2f8e60f179ff790a3525237cc50
madsaken <haffisaken@gmail.com>
2019-10-08 08:57:48 +0000

    1/3

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,34 +1,44 @@
-def location(x, y):
+def location(x, y, z):
 	"""
 	This function prints out the directions you can take and tells you if it's an invalid direction.
 	Then it returns the players decision.
 	"""
+	coins = z
 	if (x == 1 and y == 1) or (x == 2 and y == 1):
 		print("You can travel: (N)orth.")
 		answer = input("Direction: ")
 		while answer:
 			if answer == "n" or answer == "N":
-				return answer
+				return answer, coins
 			else:
 				print("Not a valid direction!")
 			answer = input("Direction: ")
 				
 	if (x == 1 and y == 2):
+		lever = input("Pull a lever (y/n): ")
+		if lever == 'y':
+			coins += 1
+			print("You received 1 coin, your total is now {}.".format(coins))
 		print("You can travel: (N)orth or (E)ast or (S)outh.")
 		answer = input("Direction: ")
 		while answer:
 			if answer == "n" or answer == "N" or answer == "e" or answer == "E" or answer == "s" or answer == "S":
-				return answer
+				return answer, coins
 			else:
 				print("Not a valid direction!")
 			answer = input("Direction: ")
 
 	if (x == 2 and y == 2) or (x == 3 and y == 3):
+		if (x == 2 and y == 2):
+			lever = input("Pull a lever (y/n): ")
+			if lever == 'y':
+				coins += 1
+				print("You received 1 coin, your total is now {}.".format(coins))
 		print("You can travel: (S)outh or (W)est.")
 		answer = input("Direction: ")
 		while answer:
 			if answer == "w" or answer == "W" or answer == "s" or answer == "S":
-				return answer
+				return answer, coins
 			else:
 				print("Not a valid direction!")
 			answer = input("Direction: ")
@@ -38,27 +48,35 @@ def location(x, y):
 		answer = input("Direction: ")
 		while answer:
 			if answer == "s" or answer == "S" or answer == "e" or answer == "E":
-				return answer
+				return answer, coins
 			else:
 				print("Not a valid direction!")
 			answer = input("Direction: ")
 
 	if (x == 2 and y == 3):
+		lever = input("Pull a lever (y/n): ")
+		if lever == 'y':
+			coins += 1
+			print("You received 1 coin, your total is now {}.".format(coins))
 		print("You can travel: (E)ast or (W)est.")
 		answer = input("Direction: ")
 		while answer:
 			if answer == "w" or answer == "W" or answer == "e" or answer == "E":
-				return answer
+				return answer, coins
 			else:
 				print("Not a valid direction!")
 			answer = input("Direction: ")
 
 	if (x == 3 and y == 2):
+		lever = input("Pull a lever (y/n): ")
+		if lever == 'y':
+			coins += 1
+			print("You received 1 coin, your total is now {}.".format(coins))
 		print("You can travel: (N)orth or (S)outh.")
 		answer = input("Direction: ")
 		while answer:
 			if answer == "n" or answer == "N" or answer == "s" or answer == "S":
-				return answer
+				return answer, coins
 			else:
 				print("Not a valid direction!")
 			answer = input("Direction: ")
@@ -80,21 +98,20 @@ def movement(answer, matrix1, matrix2):
 	elif answer == "e" or answer == "E":
 		return matrix1+1, matrix2
 
-
 pos1 = 1
 pos2 = 1
-
+totalcoins = 0
 while pos1 and pos2:
 	"""
 	This while loop constantly checks if the player has reached the goal.
 	If not it continous to give the player possible directions.
 	"""
-	direction = location(pos1, pos2)
+	direction, totalcoins = location(pos1, pos2, totalcoins)
 	
 	pos1, pos2 = movement(direction, pos1, pos2)
 
 	if pos1 == 3 and pos2 == 1:
-		print("Victory!")
+		print("Victory! Total coins {}.".format(totalcoins))
 		break
 
 
